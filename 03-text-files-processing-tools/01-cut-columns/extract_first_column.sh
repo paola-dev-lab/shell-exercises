0b886f9d72295021d498d79b1e519658c04dce6c
Paola P <dppuertol@gmail.com>
2025-12-04 18:47:21 +0000

    verifier qu'un argument et fourni et que le fichier exist

---
+++ 03-text-files-processing-tools/01-cut-columns/extract_first_column.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# objectif : extraire la première colonne d'un fichier texte
+# usage : ./extract_first_column.sh fichier.txt
+
+# work in progress
+
+# vérifier qu'un argument est fourni
+if [ -z "$1" ]; then
+echo "aucun argument n'a été fourni"
+echo "usage : extract_first_column fichier"
+exit 1
+fi
+
+# vérifier que le fichier existe
+if [ !-f "$1" ]; then
+echo "le fichier n'existe pas"
+exit 1
+fi
+
+#extraire la première colonne du fichier texte
