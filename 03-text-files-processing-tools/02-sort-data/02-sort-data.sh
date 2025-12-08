14204b7d40eb1eb467ad79fb9275a2037b2951e4
Paola P <dppuertol@gmail.com>
2025-12-08 14:15:12 +0000

    ajout du contenu du script

---
+++ 03-text-files-processing-tools/02-sort-data/02-sort-data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#objectif: trier des lignes d'un fichier non structuré en utilisant la commande sort.
+#usage: 02-sort-data.sh fichier.ext
+
+
+#le script prends un fichier en argument
+fichier=$1
+
+#vérifier qu'un argument a été donné
+if [ -z "$1" ]; then 
+echo "aucun fichier n'a pas été specifié"
+echo "spécifie un dossier" 
+echo "usage: 02-sort-data.sh fichier.ext"
+exit 1
+fi
+
+#vérifier que le fichier existe
+if [ ! -f "$1" ]; then
+echo "le fichier n'existe pas"
+exit 1
+fi
+
+#trier le fichier par valeur numérique croissante sur la colonne 2
+sort -n -k2 "$1" 
+
